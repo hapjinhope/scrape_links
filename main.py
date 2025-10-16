b144f9a8ae989a5ec34c8a1778f03577ec99b668
Коля <kola@MacBook-Pro-SweetHope.local>
2025-10-16 18:14:20 +0300

    Fix port

--- main.py
+++ main.py
@@ -271,6 +271,6 @@ async def parse_flat(request: ParseRequest):
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
 
